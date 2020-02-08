b4b341d288531694794b1ff675ff83256c4f6bb7
Micah Comfort <mcomf002@fiu.edu>
2020-02-08 15:04:06 -0500

    changed some of the settings in the book model including the length of some of the fields.

--- book/models.py
+++ book/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=100, null=False)
-    cover = models.URLField(null=False)
+    cover = models.URLField(max_length=300, null=False)
     author = models.CharField(max_length=50, null=False)
     bio = models.TextField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    genre = models.CharField(max_length=20, null=False)
+    genre = models.CharField(max_length=50, null=False)
     publisher = models.CharField(max_length=100, null=False)
     release_date = models.CharField(max_length=20, null=False)
     rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)

b59cfda10026f6d655548effc2ce16e0d3bb5180
Kenia Chang He <keniachang1997@hotmail.com>
2020-02-21 05:04:17 -0500

    Implement save for later functionality for shopping cart

--- cart/models.py
+++ cart/models.py
@@ -1,26 +1,48 @@
 from django.db import models
 from django.conf import settings
-
 from book.models import Book
 
 
-# Create your models here.
 class CartItem(models.Model):
     item = models.ForeignKey(Book, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     save_for_later = models.BooleanField(default=False, null=False)
+
+    # Compute price
+    @property
+    def cost(self):
+        return self.quantity * self.item.price
 
 
 class Cart(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     items = models.ManyToManyField(CartItem, blank=True)
 
+    # Is the list of save for later empty?
+    def is_saved_empty(self):
+        items = self.items.all()
+        for item in items:
+            if item.save_for_later is True:
+                return False
+        return True
+
+    # Is the cart empty?
+    def is_cart_empty(self):
+        items = self.items.all()
+        if items:
+            for item in items:
+                if item.save_for_later is False:
+                    return False
+        return True
+
+    # Create empty cart for user with no cart yet
     @classmethod
     def create(cls, user):
         cart = cls(user=user)
         return cart
 
 
+# Does the user have a cart?
 def has_cart(user):
     carts = Cart.objects.all()
     for cart in carts:
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Cart
 from .models import has_cart
+from .models import CartItem
 
 
 # TODO: change the login_url to the normal user login page
@@ -16,11 +18,26 @@ def cart_view(request):
 
     try:
         items = cart.items.all()
+        is_saved_empty = cart.is_saved_empty()
     except:
         items = []
+        is_saved_empty = True
+
+    is_empty_cart = cart.is_cart_empty()
 
     items_context = {
-        'items': items
+        'items': items,
+        'is_saved_empty': is_saved_empty,
+        'is_empty_cart': is_empty_cart
     }
+
+    if request.method == 'POST':
+        cart_item_ids = request.POST.getlist('saves')
+        for item_id in cart_item_ids:
+            # print(item_id)
+            cart_item = CartItem.objects.get(pk=item_id)
+            cart_item.save_for_later = True
+            cart_item.save(update_fields=['save_for_later'])
+        return HttpResponseRedirect("/cart")
 
     return render(request, "cart.html", items_context)

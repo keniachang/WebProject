6b7593e53f05d73eea32a5e8effd65a4eca4435d
antho <acast453@fiu.edu>
2020-02-29 20:31:23 -0500

    Added 10 Children's Books

--- book/views.py
+++ book/views.py
@@ -7,6 +7,6 @@ def book_detail_view(request):
     books = Book.objects.all()[:20]
     books_context = {
         'books': books
-  o  }
+    }
     return render(request, "books.html", books_context)
 

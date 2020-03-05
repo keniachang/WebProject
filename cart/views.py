08a3f160fdeeb94172b6e9c76a069f434508755e
Kenia Chang He <keniachang1997@hotmail.com>
2020-03-04 22:10:53 -0500

    Implement add to cart function for user

--- cart/views.py
+++ cart/views.py
@@ -2,17 +2,20 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Cart, CartItem, has_cart, total
+from book.models import Book
+from django.shortcuts import get_object_or_404
 
 
 # TODO: change the login_url to the normal user login page
-@login_required(login_url='/admin/')
+@login_required(login_url="/admin/")
 def cart_view(request):
     user = request.user
-    create_cart = has_cart(user)
+    create_cart = not (has_cart(user))
     if create_cart is True:
-        cart = Cart.objects.get(user=user)
+        cart = Cart(user=user)
+        cart.save()
     else:
-        cart = Cart.create(user)
+        cart = Cart.objects.get(user=user)
 
     try:
         items = cart.cartitem_set.all()
@@ -34,11 +37,12 @@ def cart_view(request):
 
     if request.method == 'POST':
         cart_item_ids = request.POST.getlist('saves')
-        for item_id in cart_item_ids:
-            # print(item_id)
-            cart_item = CartItem.objects.get(pk=item_id)
-            cart_item.save_for_later = True
-            cart_item.save(update_fields=['save_for_later'])
+        if cart_item_ids:
+            for item_id in cart_item_ids:
+                # print(item_id)
+                cart_item = CartItem.objects.get(pk=item_id)
+                cart_item.save_for_later = True
+                cart_item.save(update_fields=['save_for_later'])
 
         quantities = request.POST.getlist('quantity')
         # print(quantities)
@@ -57,10 +61,47 @@ def cart_view(request):
                 cart_item.delete()
 
         cart_item_ids = request.POST.getlist('unsaved')
-        for item_id in cart_item_ids:
-            cart_item = CartItem.objects.get(pk=item_id)
-            cart_item.save_for_later = False
-            cart_item.save(update_fields=['save_for_later'])
+        if cart_item_ids:
+            for item_id in cart_item_ids:
+                cart_item = CartItem.objects.get(pk=item_id)
+                cart_item.save_for_later = False
+                cart_item.save(update_fields=['save_for_later'])
+        return HttpResponseRedirect("/cart")
+
+    # Add book to cart by book name
+    book_name = request.GET.get('addToCart')
+    if book_name:
+        book = get_object_or_404(Book, title=book_name)
+        cart_item = cart.cartitem_set.filter(item=book)
+        if cart_item:
+            cart_item = cart_item[0]
+            cart_item.quantity += 1
+            cart_item.save(update_fields=['quantity'])
+        else:
+            CartItem.objects.create(item=book, cart=cart)
         return HttpResponseRedirect("/cart")
 
     return render(request, "cart.html", items_context)
+
+
+# TODO: change the login_url to the normal user login page
+@login_required(login_url="/admin/")
+def add_to_cart(request, book_id, current_page="/cart"):
+    user = request.user
+    create_cart = not (has_cart(user))
+    if create_cart is True:
+        cart = Cart(user=user)
+        cart.save()
+    else:
+        cart = Cart.objects.get(user=user)
+
+    book = Book.objects.get(pk=book_id)
+    cart_item = cart.cartitem_set.filter(item=book)
+    if cart_item:
+        cart_item = cart_item[0]
+        cart_item.quantity += 1
+        cart_item.save(update_fields=['quantity'])
+    else:
+        CartItem.objects.create(item=book, cart=cart)
+
+    return HttpResponseRedirect(current_page)
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import bigWishlist
+from cart.views import add_to_cart
 
-# Create your views here.
 
 @login_required(login_url='/admin/')
 def wishlist_view(request):
     user = request.user
-
     wishlist = bigWishlist.objects.get(user=user)
-
-
     wishlist_context = {
         'wishlist': wishlist
     }
+
+    # button for add to cart
+    book_id = request.GET.get('addToCart')
+    if book_id:
+        add_to_cart(request, book_id, "/wishlist")
 
     return render(request, "wishlist.html", wishlist_context)

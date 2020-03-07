24382987aa3452a618c829bd9e2d63cd3881450e
Micah Comfort <mcomf002@fiu.edu>
2020-03-06 19:45:44 -0500

    changed some functionality in comment

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
+from book.models import Book
+
+
+class Comment(models.Model):
+    book_comment = models.ForeignKey(Book, on_delete=models.CASCADE)
 
 # Create your models here.
--- comment/views.py
+++ comment/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from book.models import Book
 
 # Create your views here.
 def comment_view(request):
-    #books = Book.objects.all()[:20]
-    #comment_context = {
-    #    'books': books
-    #}
-    return render(request, "comment.html", )
+    obj = Book.objects.get(id=1)
+    context = {
+        'title': obj.title,
+        'cover': obj.cover,
+        'rating': obj.rating,
+        'comment': obj.comments
+    }
+    return render(request, "comment.html", context)

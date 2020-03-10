fb2261728bde6a67b040a39369870dcbda65d641
Andres Chinchilla <achin059@fiu.edu>
2020-03-10 19:19:07 -0400

    Added forms to update Profile data

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile
+from .models import Profile, Address, Credit_Card
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -11,5 +11,26 @@ class UserRegisterForm(UserCreationForm):
 
 
     class Meta:
-        model = User    #TODO: Change to create a profile model
+        model = User    
         fields = ['username', 'first_name', 'last_name','email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=150)
+    
+
+    class Meta:
+        model = User    
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+class PaymentInfoForm(forms.ModelForm):
+    class Meta:
+        model = Credit_Card
+        fields = ['cc_number', 'cc_expiration', 'cc_security_code']
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ['address_line', 'country', 'state', 'city', 'zip_code']
+
--- user/views.py
+++ user/views.py
@@ -1,7 +1,9 @@
 
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, PaymentInfoForm, AddressForm
+from .models import Profile
 
 def register(request):
 
@@ -19,5 +21,54 @@ def register(request):
 
     return render(request, 'user/register.html', {'form': form})
 
+@login_required
 def profile(request):
-    return render(request,'user/profile.html')
+
+    if request.method == 'POST':
+
+        user_form = UserUpdateForm(request.POST,instance=request.user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            messages.success(request, f'Account Updated!')
+            return redirect('profile') 
+    
+    else: 
+        user_form = UserUpdateForm(instance=request.user)
+      
+    
+    context = {
+        'user_form' : user_form,
+     }
+    
+    return render(request,'user/profile.html', context)
+
+@login_required
+def payment(request):
+
+    if request.method == 'POST':
+
+        pay_form = PaymentInfoForm(request.POST)
+        pay_address_form = AddressForm(request.POST)
+
+        if pay_form.is_valid() and pay_address_form.is_valid():
+            pay_form.save()
+            pay_address_form.save()
+
+            messages.success(request, f'Payment Info Added!')
+            return redirect('profile') 
+    
+    else: 
+        pay_form = PaymentInfoForm()
+        pay_address_form = AddressForm()
+
+      
+    
+    context = {
+        'pay_form' : pay_form,
+        'pay_address_form' : pay_address_form
+     }
+    
+    return render(request,'user/payment.html', context)
+
